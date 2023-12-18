040bb0817f5508f059787a45c7da0bedca9a9dce
Priti201 <Priti201>
2023-12-18 19:17:59 +0530

    created mens section

--- src/components/pages/publicPages/menProducts/Men.jsx
+++ src/components/pages/publicPages/menProducts/Men.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useBaseApi } from "../../../contextApi/BaseDomainContext";
+import axios from "axios";
+import ProductCard from "../../../productcard/ProductCard";
 
 const Men = () => {
-  return (
-    <div>Men</div>
-  )
-}
+  const [mensProducts, setMensProducts] = useState([]);
+  useEffect(() => {
+    fetchingMensProducts();
+  }, []);
+  const baseURL = useBaseApi();
+  const fetchingMensProducts = async () => {
+    try {
+      const response = await axios.get(
+        `${baseURL}/api/v1/ecommerce/clothes/products?filter={"gender":"Men"}&limit=1000`,
+        {
+          headers: {
+            projectId: "4stjj1sb1x5a",
+          },
+        }
+      );
+      console.log(response);
+      if(response.status === 200)
+      {
+        setMensProducts(response.data.data)
+      }
+    } catch (error) {}
+  };
+  return <div>
+    <ProductCard products={mensProducts}/>
+  </div>;
+};
 
-export default Men
+export default Men;
