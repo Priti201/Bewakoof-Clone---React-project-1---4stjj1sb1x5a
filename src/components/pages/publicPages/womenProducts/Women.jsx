0df1abca8b5c22004d5151a2be40ea0733ce92b9
Priti201 <Priti201>
2023-12-18 19:18:15 +0530

    created womens section

--- src/components/pages/publicPages/womenProducts/Women.jsx
+++ src/components/pages/publicPages/womenProducts/Women.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useBaseApi } from "../../../contextApi/BaseDomainContext";
+import axios from "axios";
+import ProductCard from "../../../productcard/ProductCard";
+
 
 const Women = () => {
+    const[womensProducts, setWomensProducts]= useState([]);
+    useEffect(() => {
+        fetchingWomensProducts();
+      }, []);
+      const baseURL = useBaseApi();
+      const fetchingWomensProducts = async () => {
+        try {
+          const response = await axios.get(
+            `${baseURL}/api/v1/ecommerce/clothes/products?filter={"gender":"Women"}&limit=1000`,
+            {
+              headers: {
+                projectId: "4stjj1sb1x5a",
+              },
+            }
+          );
+          console.log(response);
+          if(response.status === 200)
+          {
+            setWomensProducts(response.data.data)
+          }
+        } catch (error) {}
+    }
   return (
-    <div>Women</div>
+    <div>
+    <ProductCard products={womensProducts}/>
+  </div>
   )
 }
 
-export default Women
+export default Women;
+
+ 
