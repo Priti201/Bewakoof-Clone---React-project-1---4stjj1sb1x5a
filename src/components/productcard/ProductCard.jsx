845f6fa58ee71d4c31fdc1f14fcb9f8d75ce8eff
Priti201 <Priti201>
2023-12-19 00:31:04 +0530

    added search result title

--- src/components/productcard/ProductCard.jsx
+++ src/components/productcard/ProductCard.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './ProductCard.css'
 import Card from '../card/Card'
+import { useLocation} from 'react-router-dom';
 const ProductCard = ({products}) => {
     // console.log(data);
     // console.log('products' ,products)
+    const param = useLocation();
+    const title = param.pathname.replace("/","")
+    console.log(title);
   return (
 
-    
+    <>
+      <h3 className='search-title'>Search result for : {title}&nbsp;<span>{products.length}</span></h3>
     <div className='product-container'>
         {
             products?.map((product)=>(
@@ -14,6 +19,7 @@ const ProductCard = ({products}) => {
             ))
         }
     </div>
+    </>
   )
 }
 
